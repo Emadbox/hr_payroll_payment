d9979ee4549e5de07aa4a50fd60d43dfc40e2aee
Jburckel <jonathan.burckel@gmail.com>
2019-03-01 11:39:24 -1000

    Last test for today

--- models/res_config.py
+++ models/res_config.py
@@ -3,34 +3,18 @@
 
 from odoo import fields, models, api
 
+class ResCompany(models.Model):
+    _inherit = "res.company"
+payment_account = fields.Many2one(comodel_name='account.account', string='Debit account', domain=[('deprecated', '=', False)])
+advance_payment_account = fields.Many2one(comodel_name='account.account', string='Debit account', domain=[('deprecated', '=', False)])
+
+
+
 class HRPayrollPaymentConfigSettings(models.TransientModel):
     _name = 'hr.payroll.payment.config.settings'
     _inherit = 'res.config.settings'
 
-    payment_account = fields.Many2one('account.account', 'Debit account', domain=[('deprecated', '=', False)])
-    advance_payment_account = fields.Many2one('account.account', 'Debit account', domain=[('deprecated', '=', False)])
-
-    @api.model
-    def get_values(self):
-        res = super(HRPayrollPaymentConfigSettings, self).get_values()
-        payment_account = self.env['ir.config_parameter'].sudo().get_param(
-            'payment_account',
-            default=None
-        )
-        advance_payment_account = self.env['ir.config_parameter'].sudo().get_param(
-            'advance_payment_account',
-            default=None
-        )
-        res.update(payment_account=payment_account)
-        res.update(advance_payment_account=advance_payment_account)
-        return res
-
-    @api.multi
-    def set_values(self):
-        super(HRPayrollPaymentConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param(
-            'payment_account', self.payment_account.id
-        )
-        self.env['ir.config_parameter'].sudo().set_param(
-            'advance_payment_account', self.advance_payment_account.id
-        )
+    company_id = fields.Many2one('res.company', string='Company', required=True,
+        default=lambda self: self.env.user.company_id)
+    payment_account = fields.Many2one(comodel_name='account.account', related='company_id.payment_account', string='Debit account', domain=[('deprecated', '=', False)])
+    advance_payment_account = fields.Many2one(comodel_name='account.account', related='company_id.advance_payment_account', string='Debit account', domain=[('deprecated', '=', False)])
