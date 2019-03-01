132e9bf90cf2204cc3c78712d34fa4add8a30490
Jburckel <jonathan.burckel@gmail.com>
2019-03-01 11:31:28 -1000

    Test

--- models/res_config.py
+++ models/res_config.py
@@ -7,9 +7,30 @@ class HRPayrollPaymentConfigSettings(models.TransientModel):
     _name = 'hr.payroll.payment.config.settings'
     _inherit = 'res.config.settings'
 
-    company_id = fields.Many2one('res.company', string='Company', required=True,
-        default=lambda self: self.env.user.company_id)
     payment_account = fields.Many2one('account.account', 'Debit account', domain=[('deprecated', '=', False)])
     advance_payment_account = fields.Many2one('account.account', 'Debit account', domain=[('deprecated', '=', False)])
-    test = fields.Boolean(string='Test',
-        help="Test")
+
+    @api.model
+    def get_values(self):
+        res = super(HRPayrollPaymentConfigSettings, self).get_values()
+        payment_account = self.env['ir.config_parameter'].sudo().get_param(
+            'payment_account',
+            default=None
+        )
+        advance_payment_account = self.env['ir.config_parameter'].sudo().get_param(
+            'advance_payment_account',
+            default=None
+        )
+        res.update(payment_account=payment_account)
+        res.update(advance_payment_account=advance_payment_account)
+        return res
+
+    @api.multi
+    def set_values(self):
+        super(HRPayrollPaymentConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param(
+            'payment_account', self.payment_account.id
+        )
+        self.env['ir.config_parameter'].sudo().set_param(
+            'advance_payment_account', self.advance_payment_account.id
+        )
