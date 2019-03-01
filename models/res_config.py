f31eb01b70d0e40d1f0bbb1619bdd904d7947616
Jburckel <jonathan.burckel@gmail.com>
2019-03-01 00:39:18 -1000

    Config

--- models/res_config.py
+++ models/res_config.py
@@ -8,6 +8,5 @@ class HRPayrollPaymentConfigSettings(models.TransientModel):
     _name = 'hr.payroll.payment.config.settings'
     _inherit = 'res.config.settings'
 
-    company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.user.company_id)
     payment_account = fields.Many2one('account.account', string='Debit account', domain=[('deprecated', '=', False)])
     advance_payment_account = fields.Many2one('account.account', string='Debit account', domain=[('deprecated', '=', False)])
