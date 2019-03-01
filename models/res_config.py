6cdafe721b2230650297d613a2fe2cd46f9f7c17
Jburckel <jonathan.burckel@gmail.com>
2019-03-01 11:11:44 -1000

    Config

--- models/res_config.py
+++ models/res_config.py
@@ -3,15 +3,18 @@
 
 from odoo import fields, models
 
-class ResCompany(models.Model):
-    _inherit = "res.company"
-    payment_account = fields.Many2one(comodel_name='account.account', string='Debit account', domain=[('deprecated', '=', False)])
-    advance_payment_account = fields.Many2one(comodel_name='account.account', string='Debit account', domain=[('deprecated', '=', False)])
-
-
 class HRPayrollPaymentConfigSettings(models.TransientModel):
     _name = 'hr.payroll.payment.config.settings'
     _inherit = 'res.config.settings'
 
-    payment_account = fields.Many2one(comodel_name='account.account', related='company_id.payment_account', string='Debit account', domain=[('deprecated', '=', False)])
-    advance_payment_account = fields.Many2one(comodel_name='account.account', related='company_id.payment_account', string='Debit account', domain=[('deprecated', '=', False)])
+    payment_account = fields.Many2one('account.account', 'Debit account', domain=[('deprecated', '=', False)])
+    advance_payment_account = fields.Many2one('account.account', 'Debit account', domain=[('deprecated', '=', False)])
+
+    @api.multi
+    def execute(self):
+         values = {}
+         res = super(HRPayrollPaymentConfigSettings, self).execute()
+         values['payment_account'] = self.payment_account
+         res.update({'payment_account': values['payment_account'] or False})
+
+         return res
