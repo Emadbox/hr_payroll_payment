fcb3daec84ce2fcd359c4a7f85f9582b6140cec5
Jburckel <jonathan.burckel@gmail.com>
2019-03-01 11:00:11 -1000

    Config

--- models/res_config.py
+++ models/res_config.py
@@ -3,14 +3,15 @@
 
 from odoo import fields, models
 
+class ResCompany(models.Model):
+    _inherit = "res.company"
+    payment_account = fields.Many2one(comodel_name='account.account', string='Debit account', domain=[('deprecated', '=', False)])
+    advance_payment_account = fields.Many2one(comodel_name='account.account', string='Debit account', domain=[('deprecated', '=', False)])
+
+
 class HRPayrollPaymentConfigSettings(models.TransientModel):
     _name = 'hr.payroll.payment.config.settings'
-    _inherit = 'base.config.settings'
+    _inherit = 'res.config.settings'
 
-    payment_account = fields.Many2one('account.account', 'Debit account', domain=[('deprecated', '=', False)])
-    advance_payment_account = fields.Many2one('account.account', 'Debit account', domain=[('deprecated', '=', False)])
-
-    @api.multi
-    def set_payment_account(self):
-        self.env['ir.config_parameter'].set_param(
-            'payment_account', self.payment_account.id)
+    payment_account = fields.Many2one(comodel_name='account.account', related='company_id.payment_account', string='Debit account', domain=[('deprecated', '=', False)])
+    advance_payment_account = fields.Many2one(comodel_name='account.account', related='company_id.payment_account', string='Debit account', domain=[('deprecated', '=', False)])
