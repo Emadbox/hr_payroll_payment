0635c2022178f6cdf9b165033188d6ffde516548
Jburckel <jonathan.burckel@gmail.com>
2019-03-01 16:12:28 -1000

    payment

--- models/account_payment.py
+++ models/account_payment.py
@@ -12,6 +12,6 @@ class account_payment(models.Model):
     @api.one
     @api.depends('invoice_ids', 'payment_type', 'partner_type', 'partner_id')
     def _compute_destination_account_id(self):
-        super(account_payment, self)._compute_destination_account_id(self)
+        super(account_payment, self)._compute_destination_account_id()
         if self.employee_payment_account:
             self.destination_account_id = self.env['hr.payroll.payment.config.settings'].employee_payment_account.id
