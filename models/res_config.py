fb5b0eb546f0533125ef0426d828821dd04f02d7
Jburckel <jonathan.burckel@gmail.com>
2019-03-13 15:02:21 -1000

    Default

--- models/res_config.py
+++ models/res_config.py
@@ -2,19 +2,19 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models, api
-'''
+
 PARAMS = [
     ("payment_account", "hr.payroll.payment.config.settings.payment_account"),
     ("advance_payment_account", "hr.payroll.payment.config.settings.advance_payment_account"),
 ]
-'''
+
 class HRPayrollPaymentConfigSettings(models.TransientModel):
     _name = 'hr.payroll.payment.config.settings'
     _inherit = 'res.config.settings'
 
-    default_payment_account = fields.Many2one(comodel_name='account.account', string='Debit account', domain=[('deprecated', '=', False)])
-    default_advance_payment_account = fields.Many2one(comodel_name='account.account', string='Debit account', domain=[('deprecated', '=', False)])
-'''
+    payment_account = fields.Many2one(comodel_name='account.account', string='Debit account for payment', domain=[('deprecated', '=', False)])
+    advance_payment_account = fields.Many2one(comodel_name='account.account', string='Debit account for advance', domain=[('deprecated', '=', False)])
+
     @api.multi
     def set_params(self):
         self.ensure_one()
@@ -23,7 +23,7 @@ class HRPayrollPaymentConfigSettings(models.TransientModel):
             value = getattr(self, field_name, '').id
             self.env['ir.config_parameter'].set_param(key_name, value)
 
-    @api.multi
+    @api.model
     def get_default_params(self):
         res = {}
         for field_name, key_name in PARAMS:
@@ -31,4 +31,3 @@ class HRPayrollPaymentConfigSettings(models.TransientModel):
             if value:
                 res[field_name] = int(value)
         return res
-'''
